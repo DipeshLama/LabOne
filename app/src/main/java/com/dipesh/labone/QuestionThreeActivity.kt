8e2bf7850375c29eba3f89d905371072c1b1d7f1
DipeshLama <dipeslama32@gmail.com>
2020-12-29 11:19:43 +0545

    Question 3

---
+++ app/src/main/java/com/dipesh/labone/QuestionThreeActivity.kt
@@ -0,0 +1,11 @@
+package com.dipesh.labone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_three)
+    }
+}
